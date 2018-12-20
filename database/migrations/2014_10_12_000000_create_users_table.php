4fbbfb084aa42a1138f9e2520c29c680219970ef
newvelcomca <newvelcomca@old.velcom.ca>
2018-12-20 15:25:49 +0530

    display issue_060

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('user_id');
-            $table->string('user_name');
-            $table->string('user_email')->unique();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
